3ad81020ec8bd56100ddd44773d8adb3ea58b6da
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-10-31 23:53:03 +0300

    count

--- parser_1.py
+++ parser_1.py
@@ -1,5 +1,6 @@
 import re
 from collections import Counter
+import csv
 
 def reader(filename):
     regex = r'\d{1,3}\.\d{1,3}.\d{1,3}\.\d{1,3}'
@@ -10,10 +11,16 @@ def reader(filename):
 
 
 def count(ip_list):
-    pass
+    count = Counter(ip_list)
+    return count
 
-
-
+def write_csv(count):
+    with open('output.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        header = ['IP', 'Frequence']
+        writer.writerow(header)
+        for item in count:
+            pass
 
 if __name__ == '__main__':
     reader('2023_10_31.log')
